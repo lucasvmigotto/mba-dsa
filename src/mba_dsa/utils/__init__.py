a49b442460d0d26fda9f0c2aa4823bd3de14f0f9
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2026-01-20 17:16:29 +0000

    feat(utils): create utils module

---
+++ src/mba_dsa/utils/__init__.py
@@ -0,0 +1,5 @@
+from .data import corpus_from_dataframe as corpus_from_dataframe
+from .data import polars_to_sequence as polars_to_sequence
+from .data import shard_by_year as shard_by_year
+from .setup import setup_envvars as setup_envvars
+from .setup import setup_log as setup_log
