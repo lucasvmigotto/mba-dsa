11f8ac23e55eb5de1fd1d84754f61e272ac7ad55
Lucas <lvmigotto1@gmail.com>
2026-04-19 18:49:55 -0300

    feat(plot): create individual plot functions for scatter

--- src/mba_dsa/plot/__init__.py
+++ src/mba_dsa/plot/__init__.py
@@ -1,4 +1,4 @@
 from .graph import generate_graph as generate_graph
-from .scatter import plot_cluster_2d as plot_cluster_2d
-from .scatter import plot_cluster_3d as plot_cluster_3d
+from .scatter_2d import plot_scatter_2d as plot_scatter_2d
+from .scatter_3d import plot_scatter_3d as plot_scatter_3d
 from .wordcloud import generate_wordcloud as generate_wordcloud
--- src/mba_dsa/plot/scatter_2d.py
+++ src/mba_dsa/plot/scatter_2d.py
@@ -1,6 +1,5 @@
 from typing import Collection, Sequence
 
-import plotly.graph_objects as go
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
 from matplotlib.pyplot import subplots
@@ -8,15 +7,17 @@ from polars import DataFrame
 
 from ._utils import _PALETTE
 
+_SPINES_TO_HIDE: Collection[str] = {"top", "bottom", "left", "right"}
 
-def plot_cluster_2d(
+
+def plot_scatter_2d(
     df: DataFrame,
     col_x: str,
     col_y: str,
     col_clusters: str,
     /,
     palette: Sequence[str] | None = None,
-    spines_to_hide: Collection[str] = {},
+    spines_to_hide: Collection[str] = _SPINES_TO_HIDE,
     plot_size: tuple[int, int] = (15, 6),
     yaxis_is_visible: bool = True,
     xaxis_is_visible: bool = True,
@@ -45,40 +46,3 @@ def plot_cluster_2d(
         ax.set_title(plot_title)
 
     return fig, ax
-
-
-def plot_cluster_3d(
-    df: DataFrame,
-    col_x: str,
-    col_y: str,
-    col_z: str,
-    col_clusters: str,
-    /,
-    palette: Sequence[str] | None = None,
-):
-    _palette = (palette or _PALETTE)[
-        : len(
-            (clusters := df.get_column(col_clusters)).unique(),
-        )
-    ]
-    fig = go.Figure(
-        data=[
-            go.Scatter3d(
-                x=df.get_column(col_x),
-                y=df.get_column(col_y),
-                z=df.get_column(col_z),
-                mode="markers",
-                marker={
-                    "size": 5,
-                    "color": [_palette[i] for i in clusters],
-                    "opacity": 0.8,
-                },
-                customdata=df.select("year", "month", "lemmas"),
-                hovertemplate=(
-                    "%{customdata[1]:02}/%{customdata[0]}\n%{customdata[2]}"
-                ),
-            )
-        ]
-    )
-
-    return fig
