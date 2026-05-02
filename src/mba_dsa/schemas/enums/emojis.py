fa32d066b42bd6ccff99900d7c01c20ea73668ab
Lucas <lvmigotto1@gmail.com>
2026-05-02 19:49:38 +0000

    feat(schemas): create emoji enums

---
+++ src/mba_dsa/schemas/enums/emojis.py
@@ -0,0 +1,37 @@
+from enum import StrEnum
+
+
+class EmojisType(StrEnum):
+    KMEANS_CLUSTER = "\U00002728"  # ✨
+    COMPONENT = "\U0001f4e6"  # 📦
+    LAYOUT = "\U00001f6a7"  # 🚧
+    DISTANCE = "\U00001f6e4"  # 🛤️
+    DIRECTIONAL = "\U00002197"  # ↗️
+    LIMIT = "\U0000267e"  # ♾️
+    THRESHOLD = "\U0001f321"  # 🌡️
+    FEATURES = "\U0001f527"  # 🔧
+    MAX = "\U0001f51d"  # 🔝
+    MIN = "\U0001f90f"  # 🤏
+    TF_IDF = "\U0001f9ee"  # 🧮
+    YEAR_FILTER = "\U0001f5d3"  # 🗓️
+    CONFIG = ("\U00002699",)  # ⚙️
+    START = "\U0001f4e2"  # 📢
+    END = "\U0001f3c1"  # 🏁
+    ADD = "\U00002795"  # ➕
+    REMOVE = "\U00002796"  # ➖
+    CREATE = "\U0001fa84"  # 🪄
+    SEARCH = "\U0001f50e"  # 🔎
+    WIDTH = "\U00002194"  # ↔️
+    HEIGHT = "\U00002195"  # ↕️
+    UNIT = "\U0001f4cf"  # 📏
+    SORT = "\U0001f500"  # 🔀
+    COLOR_MAP = "\U0001f5fa"  # 🗺️
+    COLOR_MODE = "\U0001f39b"  # 🎛️
+    BACKGROUND_COLOR_MODE = "\U0001f317"  # 🌗
+    SEED = "\U0001f33e"  # 🌾
+    CLUSTER = "\U0001fadf"  # 🫟
+    GRAPH = "\U0001f578"  # 🕸️
+    WORDCLOUD = "\U00002601"  # ☁️
+    IGNORE = "\U0001f648"  # 🙈
+    COUNT = "\U0001f5a9"  # 🖩
+    FONTSIZE = "\U0001f5db"  # 🗛
